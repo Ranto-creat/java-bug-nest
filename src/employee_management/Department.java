355548e2464c4193dfca03297ea660078cdbb4f4
Ranto-Creat <hei.ranto.2@gmail.com>
2024-08-15 16:57:42 +0300

    feat: add some methodes add and show Employee

--- src/employee_management/Department.java
+++ src/employee_management/Department.java
@@ -1,15 +1,48 @@
-package employee_management;
+package java_bug_nest.src.employee_management;
 
-/**
- * TODO : a department is a collection of employees. It has a name and a set of employees in it.
- */
+import java.util.HashSet;
+import java.util.Set;
+
+/** TODO : a department is a collection of employees. It has a name and a set of employees in it. */
 public class Department {
-    /**
-     * TODO : Employees should never ever be duplicated in a single department.
-     * Please check if it already exists in the department before inserting if need be. Duplicate
-     * here means : their employee id and name are the same.
-     *
-     * TODO : for our HR officer, give a function to show all details about employees, including
-     * their pay.
-     */
+  private String DepartmentName;
+  private Set<FullTimeEmployee> FullTimeEmployees;
+  private Set<PartTimeEmployee> PartTimeEmployees;
+
+  public Department(String DepartmentName) {
+    this.DepartmentName = DepartmentName;
+    this.FullTimeEmployees = new HashSet<>();
+    this.PartTimeEmployees = new HashSet<>();
+  }
+
+  /**
+   * TODO : Employees should never ever be duplicated in a single department. Please check if it
+   * already exists in the department before inserting if need be. Duplicate here means : their
+   * employee id and name are the same.
+   *
+   * <p>TODO : for our HR officer, give a function to show all details about employees, including
+   * their pay.
+   */
+  // Polymorphisme (add, remove employee in employee type and show employee)
+  public void addEmployee(FullTimeEmployee employee) {
+    if (employee != null && !FullTimeEmployees.contains(employee)) {
+      FullTimeEmployees.add(employee);
+    }
+  }
+
+  public void addEmployee(PartTimeEmployee employee) {
+    if (employee != null && !PartTimeEmployees.contains(employee)) {
+      PartTimeEmployees.add(employee);
+    }
+  }
+
+  public void showEmployee() {
+    System.out.println("Employee in : " + DepartmentName + ":");
+    for (FullTimeEmployee employee : FullTimeEmployees) {
+      System.out.println(employee);
+    }
+    for (PartTimeEmployee employee : PartTimeEmployees) {
+      System.out.println(employee);
+    }
+  }
 }
