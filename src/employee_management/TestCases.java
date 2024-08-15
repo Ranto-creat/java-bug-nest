c913a1a1fefc766ca128f76f5179d9e2b465738f
Ranto-Creat <hei.ranto.2@gmail.com>
2024-08-15 17:00:28 +0300

    feat: add TestCases.java to test all codes

--- src/employee_management/TestCases.java
+++ src/employee_management/TestCases.java
@@ -1,28 +1,32 @@
-package employee_management;
+package java_bug_nest.src.employee_management;
 
 public class TestCases {
-    public static FullTimeEmployee fullTimeEmployeeKoto(){
-        return new FullTimeEmployee(1, "Koto", 3_000_000);
-    }
+  public static FullTimeEmployee fullTimeEmployeeKoto() {
+    return new FullTimeEmployee(1, "Koto", 3_000_000);
+  }
 
-    public static PartTimeEmployee partTimeEmployeeLita(){
-        return new PartTimeEmployee(1, "Lita", 22, 11_750);
-    }
+  public static PartTimeEmployee partTimeEmployeeLita() {
+    return new PartTimeEmployee(1, "Lita", 22, 11_750);
+  }
 
-    public static void main(String[] args) {
-        FullTimeEmployee koto = TestCases.fullTimeEmployeeKoto();
-        PartTimeEmployee lita = TestCases.partTimeEmployeeLita();
+  public static void main(String[] args) {
+    FullTimeEmployee koto = TestCases.fullTimeEmployeeKoto();
+    PartTimeEmployee lita = TestCases.partTimeEmployeeLita();
 
-        // TODO : prevent the direct access here
-        koto.monthlySalary = 6_000_000;
-        lita.hoursWorked = 1_000;
+    Department ND = new Department("New Department");
 
-        // TODO : do not allow incoherent changes on salaries (applies to full time employees too)
-        lita.hourlyRate = -10_000_000;
+    // TODO : prevent the direct access here
+    koto.monthlySalary = 6_000_000;
+    lita.hoursWorked = 1_000;
 
-        // TODO : fix the string format of both full time and part time employees
-        // NOTE : do not show sensitive data such as hourlyRate or monthlySalary here !
-        System.out.println("Details about Koto " + koto);
-        System.out.println("Details about Lita " + lita);
-    }
+    // TODO : do not allow incoherent changes on salaries (applies to full time employees too)
+    lita.hourlyRate = 200_000;
+
+    // TODO : fix the string format of both full time and part time employees
+    // NOTE : do not show sensitive data such as hourlyRate or monthlySalary here !
+    System.out.println("Details about Koto " + koto);
+    System.out.println("Details about Lita " + lita);
+    ND.showEmployee();
+    System.out.println(koto.getMonthlySalary());
+  }
 }
